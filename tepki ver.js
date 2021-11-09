9293ad85ab6950c58c1fff05c530b017e2e61faa
KayaKingg <75992606+KayaKingg@users.noreply.github.com>
2021-11-09 21:07:37 +0300

    Create tepki ver.js

---
+++ tepki ver.js
@@ -0,0 +1,8 @@
+client.on('message', async (msg) => {
+      if (msg.channel.id !== "KANALID") return;
+await msg. react('✅')
+await msg. react('❌')
+
+});
+
+//KANALID Diye Belirtilen Kısma Kanal ID Yaz
